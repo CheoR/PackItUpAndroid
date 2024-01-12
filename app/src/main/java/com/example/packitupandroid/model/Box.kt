a408ade1f31120ac22c9c6fd93f63c4abee1d672
CheoR <cheo.roman+github@gmail.com>
2024-01-12 17:26:16 -0600

    ref(model): derive isFragile and value from elements in lists

--- app/src/main/java/com/example/packitupandroid/model/Box.kt
+++ app/src/main/java/com/example/packitupandroid/model/Box.kt
@@ -4,20 +4,10 @@ data class Box(
     val id: String,
     val name: String,
     val description: String = "",
-//    val isFragile: Boolean = false, // Computed property, check if any items in box are fragile
-//    val isFragile: (Box) -> Boolean,
     val items: List<Item>,
-//    val totalValue: Double = 0.00, // Computed property, sum of values of items in the box
 ) {
     // These properties are read-only and will be computed when the `Box` object is created.
     // If the `items` list changes, create a new `Box` object to update these properties.
     val isFragile: Boolean = items.any { it.isFragile }
-    val totalValue: Double = items.sumOf { it.value }
+    val value: Double = items.sumOf { it.value }
 }
-
-//fun Box.hasFragileItem() : Boolean {
-//    /**
-//     * Check if any [Item] in [Box]'s items list are marked as fragile.
-//     */
-//    return this.items.any { it.isFragile }
-//}
--- app/src/main/java/com/example/packitupandroid/model/Collection.kt
+++ app/src/main/java/com/example/packitupandroid/model/Collection.kt
@@ -4,17 +4,8 @@ data class Collection(
     val id: String,
     val name: String,
     val description: String = "",
-//    val isFragile: Boolean = false, // Computed property, check if any boxes in collection are fragile
     val boxes: List<Box>,
-//    val totalValue: Double = 0.00  // Computed property, sum of values of items in all boxes in the collection
 ) {
     val isFragile: Boolean = boxes.any { it.isFragile }
-    val totalValue: Double = boxes.sumOf { it.totalValue }
+    val value: Double = boxes.sumOf { it.value }
 }
-
-//fun Collection.hasFragileBox() : Boolean {
-//    /**
-//     * Check if any [Box] in [Collection]'s box list are marked as fragile.
-//     */
-//    return this.boxes.any { it.isFragile }
-//}
