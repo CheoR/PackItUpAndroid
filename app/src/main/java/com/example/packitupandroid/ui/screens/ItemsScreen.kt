8530580a388d5376dc8806b4f19d62e68f6d7491
CheoR <cheo.roman+github@gmail.com>
2024-01-13 08:53:49 -0600

    ref(screens): each element type gets its own Card

--- app/src/main/java/com/example/packitupandroid/ui/screens/ItemsScreen.kt
+++ app/src/main/java/com/example/packitupandroid/ui/screens/ItemsScreen.kt
@@ -1,19 +1,16 @@
 package com.example.packitupandroid.ui.screens
 
-import BaseCard
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.packitupandroid.data.local.LocalDataSource
 import com.example.packitupandroid.model.Item
-import androidx.compose.ui.res.vectorResource
 import com.example.packitupandroid.R
-import kotlin.random.Random
+import com.example.packitupandroid.ui.components.ItemCard
 
 
 @Composable
@@ -31,17 +28,11 @@ fun ItemsScreen(
             items = cards,
             key = { it.id }
         ) {
-            BaseCard(
-                title = it.name,
-                description = it.description,
+            ItemCard(
+                item = it,
+                onUpdate = {},
+                onDelete = {},
                 onCardClick = {},
-                imageId = if(Random.nextBoolean()) R.drawable.pug else null,
-                imageVector1 = ImageVector.vectorResource(R.drawable.baseline_label_24),
-                buttonIcon = ImageVector.vectorResource(R.drawable.baseline_more_vert_24),
-                onButtonIconClick = { },
-                value = it.value,
-                isFragile = it.isFragile,
-                onCheckedChange = {},
             )
         }
     }
--- app/src/main/java/com/example/packitupandroid/ui/screens/SummaryScreen.kt
+++ app/src/main/java/com/example/packitupandroid/ui/screens/SummaryScreen.kt
@@ -1,12 +1,13 @@
 package com.example.packitupandroid.ui.screens
 
-import BaseCard
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -21,57 +22,94 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.packitupandroid.R
 import com.example.packitupandroid.data.local.LocalDataSource
-import formatValue
+import com.example.packitupandroid.model.Box
+import com.example.packitupandroid.model.Collection
+import com.example.packitupandroid.model.Item
+import com.example.packitupandroid.ui.components.CollectionCard
+import com.example.packitupandroid.ui.components.card.BaseCard
+import com.example.packitupandroid.ui.components.card.BaseCardData
+import com.example.packitupandroid.ui.components.card.ViewMode
+import com.example.packitupandroid.ui.components.formatValue
 
 @Composable
 fun SummaryScreen(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    collections: List<Collection> = emptyList(),
+    boxes: List<Box> = emptyList(),
+    items: List<Item> = emptyList(),
 ) {
+    val collections = LocalDataSource().loadCollections()
+    val boxes = LocalDataSource().loadBoxes()
+    val items = LocalDataSource().loadItems()
     Column(
         modifier = modifier,
         verticalArrangement = Arrangement
             .spacedBy(dimensionResource(R.dimen.padding_small))
     ) {
+
         BaseCard(
-            title = stringResource(R.string.collections),
-            description = "Group boxes into collections . . ",
+            data = BaseCardData.CollectionData(
+                collection = Collection(
+                    id = "collection",
+                    name = "collection",
+                    description = "consist of boxes . . ",
+                    boxes = boxes,
+                    )
+            ),
             onCardClick = {},
+            onUpdate = {},
+            onDelete = {},
             imageVector1 = ImageVector.vectorResource(R.drawable.baseline_category_24),
-            buttonIcon =  ImageVector.vectorResource(R.drawable.baseline_more_vert_24),
-            onButtonIconClick = { },
-            value = LocalDataSource().loadItems().sumOf { it.value },
-            onCheckedChange = {},
-            firstBadgeCount = LocalDataSource().loadCollections().size,
-            isFragile = LocalDataSource().loadItems().any{ it.isFragile },
-            isShowFragileAndValue = false,
+            actionIcon = ImageVector.vectorResource(R.drawable.baseline_more_vert_24)
         )
-        BaseCard(
-            title = stringResource(R.string.boxes),
-            description = "Group items into boxes . . ",
-            onCardClick = {},
-            imageVector1 = ImageVector.vectorResource(R.drawable.ic_launcher_foreground),
-            buttonIcon =  ImageVector.vectorResource(R.drawable.baseline_more_vert_24),
-            onButtonIconClick = { },
-            value = LocalDataSource().loadItems().sumOf { it.value },
-            onCheckedChange = {},
-            firstBadgeCount = LocalDataSource().loadBoxes().size,
-            isFragile = LocalDataSource().loadItems().any{ it.isFragile },
-            isShowFragileAndValue = false,
-        )
-        BaseCard(
-            title = stringResource(R.string.items),
-            dropdownOptions = LocalDataSource().loadBoxes().map { it.name },
-            description = "This is  your ",
-            onCardClick = {},
-            imageVector1 = ImageVector.vectorResource(R.drawable.baseline_label_24),
-            buttonIcon =  ImageVector.vectorResource(R.drawable.baseline_more_vert_24),
-            onButtonIconClick = { },
-            value = LocalDataSource().loadItems().sumOf { it.value },
-            onCheckedChange = {},
-            firstBadgeCount = LocalDataSource().loadItems().size,
-            isFragile = LocalDataSource().loadItems().any{ it.isFragile },
-            isShowFragileAndValue = false,
-        )
+//        BaseCard(
+//            data = ,
+//            onCardClick = {},
+//            onUpdate = {},
+//            onDelete = {},
+//            actionIcon = Icons.Filled.ArrowForward,
+//            viewMode = ViewMode.SummaryCard,
+//        )
+//       BaseCard(
+//            name = stringResource(R.string.collections),
+//            description = "Group boxes into collections . . ",
+//            onCardClick = {},
+//            imageVector1 = ImageVector.vectorResource(R.drawable.baseline_category_24),
+//            buttonIcon =  ImageVector.vectorResource(R.drawable.baseline_more_vert_24),
+//            onButtonIconClick = { },
+//            value = LocalDataSource().loadItems().sumOf { it.value },
+//            onCheckedChange = {},
+//            firstBadgeCount = LocalDataSource().loadCollections().size,
+//            isFragile = LocalDataSource().loadItems().any{ it.isFragile },
+//            isShowFragileAndValue = false,
+//        )
+//        BaseCard(
+//            title = stringResource(R.string.boxes),
+//            description = "Group items into boxes . . ",
+//            onCardClick = {},
+//            imageVector1 = ImageVector.vectorResource(R.drawable.ic_launcher_foreground),
+//            buttonIcon =  ImageVector.vectorResource(R.drawable.baseline_more_vert_24),
+//            onButtonIconClick = { },
+//            value = LocalDataSource().loadItems().sumOf { it.value },
+//            onCheckedChange = {},
+//            firstBadgeCount = LocalDataSource().loadBoxes().size,
+//            isFragile = LocalDataSource().loadItems().any{ it.isFragile },
+//            isShowFragileAndValue = false,
+//        )
+//        BaseCard(
+//            title = stringResource(R.string.items),
+//            dropdownOptions = LocalDataSource().loadBoxes().map { it.name },
+//            description = "This is  your ",
+//            onCardClick = {},
+//            imageVector1 = ImageVector.vectorResource(R.drawable.baseline_label_24),
+//            buttonIcon =  ImageVector.vectorResource(R.drawable.baseline_more_vert_24),
+//            onButtonIconClick = { },
+//            value = LocalDataSource().loadItems().sumOf { it.value },
+//            onCheckedChange = {},
+//            firstBadgeCount = LocalDataSource().loadItems().size,
+//            isFragile = LocalDataSource().loadItems().any{ it.isFragile },
+//            isShowFragileAndValue = false,
+//        )
 
         Row(
             verticalAlignment = Alignment.CenterVertically,
@@ -99,5 +137,12 @@ fun SummaryScreen(
 @Preview(showBackground = true)
 @Composable
 fun PreviewSummaryScreen() {
-    SummaryScreen()
+    val collections = LocalDataSource().loadCollections()
+    val boxes = LocalDataSource().loadBoxes()
+    val items = LocalDataSource().loadItems()
+    SummaryScreen(
+        collections = collections,
+        boxes = boxes,
+        items = items,
+    )
 }
