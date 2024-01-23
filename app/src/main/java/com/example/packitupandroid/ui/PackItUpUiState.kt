851f3a2187df693a8f170e60396a885f97996ec2
CheoR <cheo.roman+github@gmail.com>
2024-01-23 14:31:48 -0600

    build(ui): uiState with Item, Box, Collection

--- app/src/main/java/com/example/packitupandroid/ui/PackItUpUiState.kt
+++ app/src/main/java/com/example/packitupandroid/ui/PackItUpUiState.kt
@@ -1,9 +1,13 @@
 package com.example.packitupandroid.ui
 
 import com.example.packitupandroid.data.ScreenType
+import com.example.packitupandroid.model.Box
+import com.example.packitupandroid.model.Collection
+import com.example.packitupandroid.model.Item
 
 data class PackItUpUiState(
     val currentScreen: ScreenType = ScreenType.Summary,
-) {
-    // TODO: add cards here
-}
+    val items: List<Item> = emptyList(),
+    val boxes: List<Box> = emptyList(),
+    val collections: List<Collection> = emptyList(),
+)
