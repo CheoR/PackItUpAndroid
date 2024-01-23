da91005ae5583e0740aab7ddf9263aafcf1d3876
CheoR <cheo.roman+github@gmail.com>
2024-01-23 12:24:13 -0600

    ref(repository): make functions suspend functions

--- app/src/main/java/com/example/packitupandroid/repository/LocalDataRepository.kt
+++ app/src/main/java/com/example/packitupandroid/repository/LocalDataRepository.kt
@@ -6,23 +6,24 @@ import com.example.packitupandroid.model.Collection
 import com.example.packitupandroid.model.Item
 
 interface DataRepository {
-    fun loadItems(): List<Item>
-    fun loadBoxes() : List<Box>
-    fun loadCollections() : List<Collection>
+    suspend fun loadItems(): List<Item>
+    suspend fun loadBoxes(): List<Box>
+    suspend fun loadCollections(): List<Collection>
 }
 
 class LocalDataRepository (
     private val localDataSource: LocalDataSource = LocalDataSource(),
 ) : DataRepository {
 
-    override fun loadItems(): List<Item> {
+    override suspend fun loadItems(): List<Item> {
         return localDataSource.loadItems()
     }
-    override fun loadBoxes(): List<Box> {
+
+    override suspend fun loadBoxes(): List<Box> {
         return localDataSource.loadBoxes()
     }
 
-    override fun loadCollections(): List<Collection> {
+    override suspend fun loadCollections(): List<Collection> {
         return localDataSource.loadCollections()
     }
 }
