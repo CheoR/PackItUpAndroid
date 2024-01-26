3796965c7ab2a494d7784462040412b7dca470c2
CheoR <cheo.roman+github@gmail.com>
2024-01-25 21:05:49 -0600

    build(common): custom Buttons

---
+++ app/src/main/java/com/example/packitupandroid/ui/components/common/AddConfirmCancelButton.kt
@@ -0,0 +1,124 @@
+package com.example.packitupandroid.ui.components.common
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Cancel
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+//sealed class ButtonAction {
+//    data class Add(
+//        val label: String = "add",
+//        val icon: ImageVector = Icons.Default.Add,
+//        val color: Color = Color.Gray,
+//    ) : ButtonAction()
+//
+//    data class Confirm(
+//        val label: String = "confirm",
+//        val icon: ImageVector = Icons.Default.Check,
+//        val color: Color = Color.Green,
+//    ) : ButtonAction()
+//
+//    data class Cancel(
+//        val label: String = "cancel",
+//        val icon: ImageVector = Icons.Default.Cancel,
+//        val color: Color = Color.Red,
+//    ) : ButtonAction()
+//}
+
+sealed class ButtonType {
+    object Add : ButtonType()
+    object Cancel : ButtonType()
+    object Confirm : ButtonType()
+}
+
+
+@Composable
+fun AddConfirmCancelButton(
+    modifier: Modifier = Modifier,
+    count: Int? = 0,
+    button: ButtonType,
+    onClick: (Int?) -> Unit,
+) {
+    val (label, icon, color) = when(button) {
+        is ButtonType.Add -> Triple("add", Icons.Default.Add, MaterialTheme.colorScheme.onPrimaryContainer)
+        is ButtonType.Confirm ->  Triple("confirm", Icons.Default.Check, MaterialTheme.colorScheme.onSecondaryContainer)
+        is ButtonType.Cancel ->  Triple("cancel", Icons.Default.Cancel, MaterialTheme.colorScheme.error)
+    }
+
+    Row(
+        modifier = modifier
+            .height(32.dp)
+            .fillMaxWidth()
+            .background(color = Color.Blue),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Button(
+            modifier = modifier
+                .fillMaxWidth()
+                .background(color = color),
+            shape = RoundedCornerShape(4.dp),
+            onClick = { onClick(count) },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = color,
+                contentColor = MaterialTheme.colorScheme.primary,
+            ),
+        ) {
+            Icon(
+                imageVector = icon,
+                contentDescription = label,
+                tint = MaterialTheme.colorScheme.onPrimary,
+            )
+            Text(
+                text = label,
+                style = MaterialTheme.typography.labelSmall,
+                color = MaterialTheme.colorScheme.onPrimary,
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewAddConfirmCancelButtonAdd() {
+    AddConfirmCancelButton(
+        button = ButtonType.Add,
+        onClick = { },
+        count = 5,
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewAddConfirmCancelButtonConfirm() {
+    AddConfirmCancelButton(
+        button = ButtonType.Confirm,
+        onClick = { },
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewAddConfirmCancelButtonCancel() {
+    AddConfirmCancelButton(
+        button = ButtonType.Cancel,
+        onClick = { },
+    )
+}
