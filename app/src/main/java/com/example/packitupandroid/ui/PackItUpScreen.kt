fa6b15aeb5642dc6cb4831cbcffa983048ddf63a
CheoR <cheo.roman+github@gmail.com>
2024-01-28 16:52:03 -0600

    todo: review and delete if not using files

--- app/src/main/java/com/example/packitupandroid/ui/PackItUpScreen.kt
+++ app/src/main/java/com/example/packitupandroid/ui/PackItUpScreen.kt
@@ -31,151 +31,151 @@ import com.example.packitupandroid.data.local.LocalDataSource
 import com.example.packitupandroid.ui.utils.PackItUpContentType
 import com.example.packitupandroid.ui.utils.PackItUpNavigationType
 
-@Composable
-fun PackItUpScreen(
-    navigationType: PackItUpNavigationType,
-    contentType: PackItUpContentType,
-    uiState: PackItUpUiState,
-    onTabPressed: (ScreenType) -> Unit,
-    modifier: Modifier = Modifier
-) {
-    val navigationItemContentList = listOf(
-        NavigationItemContent(
-            screenType = ScreenType.Summary,
-            icon = Icons.Default.Home,
-            text = stringResource(id = R.string.summary)
-        ),
-        NavigationItemContent(
-            screenType = ScreenType.Collections,
-            icon = ImageVector.vectorResource(R.drawable.baseline_category_24),
-            text = stringResource(id = R.string.collections)
-        ),
-        NavigationItemContent(
-            screenType = ScreenType.Boxes,
-            icon = ImageVector.vectorResource(R.drawable.ic_launcher_foreground),
-            text = stringResource(id = R.string.boxes)
-        ),
-        NavigationItemContent(
-            screenType = ScreenType.Items,
-            icon = ImageVector.vectorResource(R.drawable.baseline_label_24),
-            text = stringResource(id = R.string.items)
-        ),
-    )
-
-    PackItUpAppContent(
-        navigationType = navigationType,
-        contentType = contentType,
-        uiState = uiState,
-        onTabPressed = onTabPressed,
-        navigationItemContentList = navigationItemContentList,
-        modifier = modifier
-    )
-}
-
-@Composable
-private fun PackItUpAppContent(
-    navigationType: PackItUpNavigationType,
-    contentType: PackItUpContentType,
-    uiState: PackItUpUiState,
-    onTabPressed: ((ScreenType) -> Unit),
-    navigationItemContentList: List<NavigationItemContent>,
-    modifier: Modifier = Modifier,
-) {
-    Box(modifier = modifier) {
-        Row(modifier = Modifier.fillMaxSize()) {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(MaterialTheme.colorScheme.inverseOnSurface)
-            ) {
-
-                PackItUpListOnlyContent(
-                    modifier = Modifier.weight(1f)
-                        .padding(
-                            horizontal = dimensionResource(R.dimen.padding_small)
-                        ),
-                    uiState =  uiState,
-                    onClick = {},
-                )
-
-                AnimatedVisibility(visible = navigationType == PackItUpNavigationType.BOTTOM_NAVIGATION) {
-                    val bottomNavigationContentDescription = stringResource(R.string.navigation_bottom)
-                    PackItUpBottomNavigationBar(
-                        currentTab = uiState.currentScreen,
-                        onTabPressed = onTabPressed,
-                        navigationItemContentList = navigationItemContentList,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .testTag(bottomNavigationContentDescription),
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-private fun PackItUpBottomNavigationBar(
-    currentTab: ScreenType,
-    onTabPressed: ((ScreenType) -> Unit),
-    navigationItemContentList: List<NavigationItemContent>,
-    modifier: Modifier = Modifier
-) {
-    NavigationBar(modifier = modifier) {
-        for (navItem in navigationItemContentList) {
-            NavigationBarItem(
-                selected = currentTab == navItem.screenType,
-                onClick = { onTabPressed(navItem.screenType) },
-                icon = {
-                    BadgedBox(
-                        modifier = Modifier,
-                        badge = {
-                            Badge(
-                                modifier = Modifier
-                            ) {
-                                Text("0")
-                            }
-                        }
-                    ) {
-                        Icon(
-                            imageVector = navItem.icon,
-                            contentDescription = navItem.text
-                        )
-                    }
-                }
-            )
-        }
-    }
-}
-private data class NavigationItemContent(
-    val screenType: ScreenType,
-    val icon: ImageVector,
-    val text: String
-)
-
-
-@Preview
-@Composable
-fun PreviewSummaryScreen(
-    localDataSource: LocalDataSource = LocalDataSource(),
-) {
-    val currentScreen = ScreenType.Summary
-    val items = localDataSource.loadItems()
-    val boxes = localDataSource.loadBoxes()
-    val collections = localDataSource.loadCollections()
-
-    val uiState = PackItUpUiState(
-        currentScreen = currentScreen,
-        items = items,
-        boxes = boxes,
-        collections = collections,
-    )
-
-    PackItUpScreen(
-        navigationType = PackItUpNavigationType.BOTTOM_NAVIGATION,
-        contentType = PackItUpContentType.LIST_ONLY,
-        uiState = uiState,
-        onTabPressed = {},
-        modifier = Modifier,
-    )
-}
+//@Composable
+//fun PackItUpScreen(
+//    navigationType: PackItUpNavigationType,
+//    contentType: PackItUpContentType,
+//    uiState: PackItUpUiState,
+//    onTabPressed: (ScreenType) -> Unit,
+//    modifier: Modifier = Modifier
+//) {
+//    val navigationItemContentList = listOf(
+//        NavigationItemContent(
+//            screenType = ScreenType.Summary,
+//            icon = Icons.Default.Home,
+//            text = stringResource(id = R.string.summary)
+//        ),
+//        NavigationItemContent(
+//            screenType = ScreenType.Collections,
+//            icon = ImageVector.vectorResource(R.drawable.baseline_category_24),
+//            text = stringResource(id = R.string.collections)
+//        ),
+//        NavigationItemContent(
+//            screenType = ScreenType.Boxes,
+//            icon = ImageVector.vectorResource(R.drawable.ic_launcher_foreground),
+//            text = stringResource(id = R.string.boxes)
+//        ),
+//        NavigationItemContent(
+//            screenType = ScreenType.Items,
+//            icon = ImageVector.vectorResource(R.drawable.baseline_label_24),
+//            text = stringResource(id = R.string.items)
+//        ),
+//    )
+//
+//    PackItUpAppContent(
+//        navigationType = navigationType,
+//        contentType = contentType,
+//        uiState = uiState,
+//        onTabPressed = onTabPressed,
+//        navigationItemContentList = navigationItemContentList,
+//        modifier = modifier
+//    )
+//}
+//
+//@Composable
+//private fun PackItUpAppContent(
+//    navigationType: PackItUpNavigationType,
+//    contentType: PackItUpContentType,
+//    uiState: PackItUpUiState,
+//    onTabPressed: ((ScreenType) -> Unit),
+//    navigationItemContentList: List<NavigationItemContent>,
+//    modifier: Modifier = Modifier,
+//) {
+//    Box(modifier = modifier) {
+//        Row(modifier = Modifier.fillMaxSize()) {
+//            Column(
+//                modifier = Modifier
+//                    .fillMaxSize()
+//                    .background(MaterialTheme.colorScheme.inverseOnSurface)
+//            ) {
+//
+//                PackItUpListOnlyContent(
+//                    modifier = Modifier.weight(1f)
+//                        .padding(
+//                            horizontal = dimensionResource(R.dimen.padding_small)
+//                        ),
+//                    uiState =  uiState,
+//                    onClick = {},
+//                )
+//
+//                AnimatedVisibility(visible = navigationType == PackItUpNavigationType.BOTTOM_NAVIGATION) {
+//                    val bottomNavigationContentDescription = stringResource(R.string.navigation_bottom)
+//                    PackItUpBottomNavigationBar(
+//                        currentTab = uiState.currentScreen,
+//                        onTabPressed = onTabPressed,
+//                        navigationItemContentList = navigationItemContentList,
+//                        modifier = Modifier
+//                            .fillMaxWidth()
+//                            .testTag(bottomNavigationContentDescription),
+//                    )
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//@Composable
+//private fun PackItUpBottomNavigationBar(
+//    currentTab: ScreenType,
+//    onTabPressed: ((ScreenType) -> Unit),
+//    navigationItemContentList: List<NavigationItemContent>,
+//    modifier: Modifier = Modifier
+//) {
+//    NavigationBar(modifier = modifier) {
+//        for (navItem in navigationItemContentList) {
+//            NavigationBarItem(
+//                selected = currentTab == navItem.screenType,
+//                onClick = { onTabPressed(navItem.screenType) },
+//                icon = {
+//                    BadgedBox(
+//                        modifier = Modifier,
+//                        badge = {
+//                            Badge(
+//                                modifier = Modifier
+//                            ) {
+//                                Text("0")
+//                            }
+//                        }
+//                    ) {
+//                        Icon(
+//                            imageVector = navItem.icon,
+//                            contentDescription = navItem.text
+//                        )
+//                    }
+//                }
+//            )
+//        }
+//    }
+//}
+//private data class NavigationItemContent(
+//    val screenType: ScreenType,
+//    val icon: ImageVector,
+//    val text: String
+//)
+//
+//
+//@Preview
+//@Composable
+//fun PreviewSummaryScreen(
+//    localDataSource: LocalDataSource = LocalDataSource(),
+//) {
+//    val currentScreen = ScreenType.Summary
+//    val items = localDataSource.loadItems()
+//    val boxes = localDataSource.loadBoxes()
+//    val collections = localDataSource.loadCollections()
+//
+//    val uiState = PackItUpUiState(
+//        currentScreen = currentScreen,
+//        items = items,
+//        boxes = boxes,
+//        collections = collections,
+//    )
+//
+//    PackItUpScreen(
+//        navigationType = PackItUpNavigationType.BOTTOM_NAVIGATION,
+//        contentType = PackItUpContentType.LIST_ONLY,
+//        uiState = uiState,
+//        onTabPressed = {},
+//        modifier = Modifier,
+//    )
+//}
--- app/src/main/java/com/example/packitupandroid/ui/PackItUpScreenContent.kt
+++ app/src/main/java/com/example/packitupandroid/ui/PackItUpScreenContent.kt
@@ -8,39 +8,39 @@ import com.example.packitupandroid.data.ScreenType
 import com.example.packitupandroid.data.local.LocalDataSource
 import com.example.packitupandroid.ui.screens.SummaryScreen
 
-@Composable
-fun PackItUpListOnlyContent(
-    modifier: Modifier = Modifier,
-    uiState: PackItUpUiState,
-    onClick: () -> Unit,
-) {
-    Column() {
-        SummaryScreen(
-            uiState = uiState,
-            onClick = onClick,
-        )
-    }
-}
-
-@Preview
-@Composable
-fun PreviewPackItUpListOnlyContent(
-    localDataSource: LocalDataSource = LocalDataSource(),
-) {
-    val currentScreen = ScreenType.Summary
-    val items = localDataSource.loadItems()
-    val boxes = localDataSource.loadBoxes()
-    val collections = localDataSource.loadCollections()
-
-    val uiState = PackItUpUiState(
-        currentScreen = currentScreen,
-        items = items,
-        boxes = boxes,
-        collections = collections,
-    )
-
-    PackItUpListOnlyContent(
-        uiState = uiState,
-        onClick = {},
-    )
-}
+//@Composable
+//fun PackItUpListOnlyContent(
+//    modifier: Modifier = Modifier,
+//    uiState: PackItUpUiState,
+//    onClick: () -> Unit,
+//) {
+//    Column() {
+//        SummaryScreen(
+//            uiState = uiState,
+//            onClick = onClick,
+//        )
+//    }
+//}
+//
+//@Preview
+//@Composable
+//fun PreviewPackItUpListOnlyContent(
+//    localDataSource: LocalDataSource = LocalDataSource(),
+//) {
+//    val currentScreen = ScreenType.Summary
+//    val items = localDataSource.loadItems()
+//    val boxes = localDataSource.loadBoxes()
+//    val collections = localDataSource.loadCollections()
+//
+//    val uiState = PackItUpUiState(
+//        currentScreen = currentScreen,
+//        items = items,
+//        boxes = boxes,
+//        collections = collections,
+//    )
+//
+//    PackItUpListOnlyContent(
+//        uiState = uiState,
+//        onClick = {},
+//    )
+//}
