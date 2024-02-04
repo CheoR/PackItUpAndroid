68d1d5cc4698fa39c74c2ba48992e0d9d3aa415b
CheoR <cheo.roman+github@gmail.com>
2024-02-03 21:02:30 -0600

    chore(navigation): label navigation icons

--- app/src/main/java/com/example/packitupandroid/ui/navigation/PackItUpComponents.kt
+++ app/src/main/java/com/example/packitupandroid/ui/navigation/PackItUpComponents.kt
@@ -1,10 +1,11 @@
 package com.example.packitupandroid.ui.navigation
 
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -21,6 +22,13 @@ fun PackItUpBottomNavigationBar(
 ) {
     NavigationBar(
         modifier = modifier,
+//            .fillMaxWidth(),
+//            .height(80.dp)
+//            .padding(
+//                top = 12.dp,
+//                bottom = 16.dp,
+//            ),
+        contentColor = MaterialTheme.colorScheme.onPrimaryContainer,
     ) {
         TOP_LEVEL_DESTINATIONS.forEach { packItUpDestination ->
             val selectedIcon = when(packItUpDestination.selectedIcon) {
@@ -28,14 +36,32 @@ fun PackItUpBottomNavigationBar(
                 else -> ImageVector.vectorResource(id=packItUpDestination.selectedIcon as Int)
             }
             NavigationBarItem(
-                modifier = Modifier.height(dimensionResource(R.dimen.navigation_bar_item_height)),
+                modifier = Modifier,
+//                    .height(dimensionResource(R.dimen.navigation_bar_item_height))
                 selected = selectedDestination == packItUpDestination.route,
                 onClick = { navigateToTopLevelDestination(packItUpDestination) },
                 icon = {
                     Icon(
                         imageVector = selectedIcon,
-                        contentDescription = stringResource(id = packItUpDestination.iconTextId),
+                        contentDescription = stringResource(packItUpDestination.iconTextId),
                         modifier = Modifier.size(dimensionResource(R.dimen.image_size_small)),
+                        tint = if (selectedDestination == packItUpDestination.route) {
+                            MaterialTheme.colorScheme.primary
+                        } else {
+                            MaterialTheme.colorScheme.onSurfaceVariant
+                        }
+                    )
+                },
+                label = {
+                    Text(
+                        text = packItUpDestination.route.toString(),
+                        color = if (selectedDestination == packItUpDestination.route) {
+                            MaterialTheme.colorScheme.primary
+                        } else {
+                            MaterialTheme.colorScheme.onSurfaceVariant
+                        },
+                        style = MaterialTheme.typography.bodySmall,
+//                        modifier = Modifier.padding(top = 4.dp)
                     )
                 }
             )
