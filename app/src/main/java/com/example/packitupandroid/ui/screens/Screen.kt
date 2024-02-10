fc57a213fc16cd16e5895088b3e29ffdc292d38a
CheoR <cheo.roman+github@gmail.com>
2024-02-10 14:35:14 -0600

    build(screens): generic screen for lazylist of object instances and Vounter

---
+++ app/src/main/java/com/example/packitupandroid/ui/screens/Screen.kt
@@ -0,0 +1,39 @@
+package com.example.packitupandroid.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import com.example.packitupandroid.R
+import com.example.packitupandroid.model.BaseCardData
+import com.example.packitupandroid.ui.components.counter.Counter
+
+@Composable
+fun <T: BaseCardData> Screen(
+    elements: List<T>,
+    onClick: (T, Int?) -> Unit,
+    updateElement: (T) -> Unit,
+    destroyElement: (T) -> Unit,
+    card: @Composable (T, (T) -> Unit, (T) -> Unit) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column(modifier = modifier) {
+        LazyColumn(
+            modifier = modifier.weight(1f),
+            verticalArrangement = Arrangement.spacedBy(
+                dimensionResource(R.dimen.padding_small)
+            )
+        ) {
+            items(
+                items = elements,
+                key = { it.id }
+            ) { element ->
+                card(element, updateElement, destroyElement)
+            }
+        }
+        Counter(type = elements.firstOrNull(), onClick = onClick)
+    }
+}
