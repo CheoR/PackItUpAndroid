40009b0b003b88741383963d7a5c1e590b63311e
CheoR <cheo.roman+github@gmail.com>
2024-02-15 13:51:58 -0600

    ref(card): remove unused variables

--- app/src/main/java/com/example/packitupandroid/ui/components/card/ActionColumn.kt
+++ app/src/main/java/com/example/packitupandroid/ui/components/card/ActionColumn.kt
@@ -1,6 +1,5 @@
 package com.example.packitupandroid.ui.components.card
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,47 +10,39 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.CameraAlt
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material3.Card
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MenuDefaults
-import androidx.compose.material3.MenuItemColors
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import com.example.packitupandroid.data.local.LocalDataSource
 import com.example.packitupandroid.model.BaseCardData
-import com.example.packitupandroid.model.Item
 import com.example.packitupandroid.model.Summary
 import com.example.packitupandroid.ui.components.EditCard
 
 @Composable
 fun ActionColumn(
     data: BaseCardData,
-    onClick: () -> Unit,
-    onSave: () -> Unit,
-    onCancel: () -> Unit,
-    onEdit: (BaseCardData) -> Unit,
     isExpanded: MutableState<Boolean>,
     isShowEditCard: MutableState<Boolean>,
+    onClick: () -> Unit,
+    onCancel: () -> Unit,
+    onEdit: (BaseCardData) -> Unit,
     modifier: Modifier = Modifier,
+    onDestroy: () -> Unit? = {},
     editMode: EditMode = EditMode.NoEdit,
     cardType: CardType = CardType.Default,
-    onDestroy: () -> Unit? = {},
 ) {
     val actionIcon: ActionColumnState = when(cardType) {
         is CardType.Summary -> ActionColumnState.RightArrow
@@ -66,7 +57,6 @@ fun ActionColumn(
             EditCard(
                 data = data,
                 onEdit = onEdit,
-                onSave = onSave,
                 onCancel = onCancel,
             )
         }
@@ -176,69 +166,72 @@ fun ActionColumn(
     }
 }
 
-//@Preview(
-//    showBackground = true,
-//    group="Summary",
-//)
-//@Composable
-//fun PreviewActionColumnSummaryCardNoEdit() {
-//    val summary = Summary(
-//        id = "summary",
-//        name = "summary",
-//        description = "summary",
-//    )
-//
-//    val isShowEditCard = remember { mutableStateOf(false) }
-//    val isExpanded = remember { mutableStateOf(false) }
-////        editMode = EditMode.NoEdit,
-//    ActionColumn(
-//        data = summary,
-//        onUpdate = {},
-//        onClick = {},
-//        isShowEditCard = isShowEditCard,
-//        isExpanded = isExpanded,
-////        editMode = EditMode.NoEdit,
-//    )
-//}
-//
-//@Preview(
-//    showBackground = true,
-//    group="Default",
-//)
-//@Composable
-//fun PreviewActionColumnDefaultCardNoEdit(
-//    localDataSource: LocalDataSource = LocalDataSource(),
-//) {
-//    val collection = localDataSource.loadCollections().first()
-//    val isShowEditCard = remember { mutableStateOf(false) }
-//    val isExpanded = remember { mutableStateOf(false) }
-//    ActionColumn(
-//        data = collection,
-//        onUpdate = {},
-//        onClick = {},
-//        isShowEditCard = isShowEditCard,
-//        isExpanded = isExpanded,
-////        editMode = EditMode.NoEdit,
-//    )
-//}
-//
-//@Preview(
-//    showBackground = true,
-//    group="Edit",
-//)
-//@Composable
-//fun PreviewActionColumnEditCardEdit(
-//    localDataSource: LocalDataSource = LocalDataSource(),
-//) {
-//    val collection = localDataSource.loadCollections().first()
-//    val isShowEditCard = remember { mutableStateOf(false) }
-//    val isExpanded = remember { mutableStateOf(false) }
-//    ActionColumn(
-//        data = collection,
-//        onUpdate = {},
-//        onClick = {},
-//        isShowEditCard = isShowEditCard,
-//        isExpanded = isExpanded,
-////        editMode = EditMode.Edit,
-//    )
-//}
+@Preview(
+    showBackground = true,
+    group="Summary",
+)
+@Composable
+fun PreviewActionColumnSummaryCardNoEdit() {
+    val summary = Summary(
+        id = "summary",
+        name = "summary",
+        description = "summary",
+    )
+
+    // to avoid
+    // Creating a state object during composition without using remember More... (Ctrl+F1)
+    val isShowEditCard = remember { mutableStateOf(false) }
+    val isExpanded = remember { mutableStateOf(false) }
+    ActionColumn(
+        data = summary,
+        onClick = {},
+        onEdit = {},
+        onCancel = {},
+        isShowEditCard = isShowEditCard,
+        isExpanded = isExpanded,
+        cardType = CardType.Summary,
+    )
+}
+
+@Preview(
+    showBackground = true,
+    group="Default",
+)
+@Composable
+fun PreviewActionColumnDefaultCardNoEdit(
+    localDataSource: LocalDataSource = LocalDataSource(),
+) {
+    val collection = localDataSource.loadCollections().first()
+    val isShowEditCard = remember { mutableStateOf(false) }
+    val isExpanded = remember { mutableStateOf(false) }
+    ActionColumn(
+        data = collection,
+        onClick = {},
+        onEdit = {},
+        onCancel = {},
+        isShowEditCard = isShowEditCard,
+        isExpanded = isExpanded,
+    )
+}
+
+@Preview(
+    showBackground = true,
+    group="Edit",
+)
+@Composable
+fun PreviewActionColumnEditCardEdit(
+    localDataSource: LocalDataSource = LocalDataSource(),
+) {
+    val collection = localDataSource.loadCollections().first()
+    val isShowEditCard = remember { mutableStateOf(false) }
+    val isExpanded = remember { mutableStateOf(false) }
+    ActionColumn(
+        data = collection,
+        onClick = {},
+        onEdit = {},
+        onCancel = {},
+        isShowEditCard = isShowEditCard,
+        isExpanded = isExpanded,
+        editMode = EditMode.Edit,
+    )
+}
