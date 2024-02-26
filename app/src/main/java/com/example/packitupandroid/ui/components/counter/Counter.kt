6106801de0a9f27ecb63bcd46b8d02db2f9f1087
CheoR <cheo.roman+github@gmail.com>
2024-02-26 15:08:11 -0600

    ref(counter): add semantics to counter value

--- app/src/main/java/com/example/packitupandroid/ui/components/counter/Counter.kt
+++ app/src/main/java/com/example/packitupandroid/ui/components/counter/Counter.kt
@@ -24,6 +24,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.packitupandroid.model.BaseCardData
@@ -60,7 +62,10 @@ fun <T: BaseCardData> Counter(
                 Icon(imageVector = Icons.Default.Remove, contentDescription = "subtract")
             }
             TextButton(
-                modifier = buttonModifier,
+                modifier = buttonModifier
+                    .semantics {
+                               contentDescription = "Counter Value"
+                    },
                 onClick = {},
                 enabled = false,
             ) {
