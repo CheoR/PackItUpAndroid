08f882d2ebfa9b100ecea2b369e6763e36f0e158
CheoR <cheo.roman+github@gmail.com>
2024-02-27 14:47:45 -0600

    build(gradle): bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,12 @@ buildscript {
     extra.apply {
         set("lifecycle_version", "2.7.0")
         set("navigation_version", "2.7.7")
-        set("compose_compiler_version", "1.5.1")
+        set("compose_compiler_version", "1.5.10")
     }
 }
 
 plugins {
     id("com.android.library") version "8.2.2" apply false
     id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
