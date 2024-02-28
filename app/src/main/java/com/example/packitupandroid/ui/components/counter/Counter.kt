71a15edd556f96a92015bcf26963bb70516af3cd
CheoR <cheo.roman+github@gmail.com>
2024-02-28 11:22:54 -0600

    ref(counter): add semantics for testing

--- app/src/main/java/com/example/packitupandroid/ui/components/counter/Counter.kt
+++ app/src/main/java/com/example/packitupandroid/ui/components/counter/Counter.kt
@@ -24,6 +24,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.packitupandroid.model.BaseCardData
@@ -60,7 +63,10 @@ fun <T: BaseCardData> Counter(
                 Icon(imageVector = Icons.Default.Remove, contentDescription = "subtract")
             }
             TextButton(
-                modifier = buttonModifier,
+                modifier = buttonModifier
+                    .semantics {
+                               contentDescription = "Counter Value"
+                    },
                 onClick = {},
                 enabled = false,
             ) {
@@ -70,7 +76,8 @@ fun <T: BaseCardData> Counter(
                 )
             }
             IconButton(
-                modifier = buttonModifier,
+                modifier = buttonModifier
+                    .testTag("ADD"),
                 onClick = { count++ },
             ) {
                 Icon(imageVector = Icons.Default.Add, contentDescription = "add")
