783ca6fe3459f6836096ed89df5fad80c751b719
CheoR <cheo.roman+github@gmail.com>
2024-03-24 12:43:27 -0500

    build(data): ItemDao

---
+++ app/src/main/java/com/example/packitupandroid/data/ItemDao.kt
@@ -0,0 +1,30 @@
+package com.example.packitupandroid.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.packitupandroid.model.Item
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ItemDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(item: Item)
+
+    @Update
+    suspend fun update(item: Item)
+
+    @Delete
+    suspend fun delete(item: Item)
+
+    // recommended to use Flow in persistence layer
+    @Query("SELECT * FROM items WHERE id = :id")
+    fun getItem(id: String): Flow<Item>
+
+    @Query("SELECT * from items ORDER BY name ASC")
+    fun getAllItems(): Flow<List<Item>>
+
+}
