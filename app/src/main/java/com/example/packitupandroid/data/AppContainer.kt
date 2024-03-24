57c5219d6c4affa017ff6b5f81ddd9e6f7659e0c
CheoR <cheo.roman+github@gmail.com>
2024-03-24 14:29:54 -0500

    build(data): instantiate db andd pass Item Dao instance to offline item respository

--- app/src/main/java/com/example/packitupandroid/data/AppContainer.kt
+++ app/src/main/java/com/example/packitupandroid/data/AppContainer.kt
@@ -1,15 +1,32 @@
 package com.example.packitupandroid.data
 
+import android.content.Context
 import com.example.packitupandroid.repository.LocalDataRepository
 
+/**
+ * App container for Dependency injection.
+ */
 interface AppContainer {
     val localDataRepository: LocalDataRepository
+    val itemsRepository: ItemsRepository
 }
 
-class DefaultAppContainer : AppContainer {
+/**
+ * [AppContainer] implementation provides [OfflineItemsRepository] instance and [LocalDataRepository]
+ * instance
+ */
+class DefaultAppContainer(private val context: Context) : AppContainer {
 
     override val localDataRepository: LocalDataRepository by lazy {
         LocalDataRepository()
     }
-
+    /**
+     * [ItemsRepository] implementation
+     */
+    override val itemsRepository: ItemsRepository by lazy {
+        // Call getDatabase() to instantiate database instance
+        // on AppDatabase class passing in context and call
+        // .itemDao() to create Dao instance
+        OfflineItemsRepository(AppDatabase.getDatabase(context).itemDao())
+    }
 }
