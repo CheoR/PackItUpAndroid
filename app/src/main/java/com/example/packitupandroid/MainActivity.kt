3fd76021f40e2bd92223bf71f5f449130c7b2a8d
CheoR <cheo.roman+github@gmail.com>
2024-04-16 17:51:52 -0500

    ref(app): move uiState closer to navgraph with viewModel

--- app/src/main/java/com/example/packitupandroid/MainActivity.kt
+++ app/src/main/java/com/example/packitupandroid/MainActivity.kt
@@ -6,34 +6,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
-import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.DpSize
-import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.example.packitupandroid.data.source.local.LocalDataSource
 import com.example.packitupandroid.ui.PackItUpApp
-import com.example.packitupandroid.ui.PackItUpUiState
-import com.example.packitupandroid.ui.PackItUpViewModel
-import com.example.packitupandroid.ui.PackItUpViewModelProvider
-import com.example.packitupandroid.ui.navigation.PackItUpRoute
 import com.example.packitupandroid.ui.theme.PackItUpAndroidTheme
 
 class MainActivity : ComponentActivity() {
-    private val viewModel: PackItUpViewModel by viewModels {
-        PackItUpViewModelProvider.Factory
-    }
+//    private val viewModel: ItemsScreenViewModel by viewModels {
+//        PackItUpViewModelProvider.Factory
+//    }
     @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
@@ -48,7 +35,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             PackItUpAndroidTheme {
                 val windowSize = calculateWindowSizeClass(this)
-                val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+//                val uiState by viewModel.uiState.collectAsStateWithLifecycle()
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -56,7 +43,7 @@ class MainActivity : ComponentActivity() {
                 ) {
                     PackItUpApp(
                         windowSize = windowSize,
-                        uiState = uiState,
+//                        uiState = uiState,
                     )
                 }
             }
@@ -79,28 +66,29 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
-@Preview(showBackground = true)
-@Composable
-fun PreviewPackItUp(
-    localDataSource: LocalDataSource = LocalDataSource(),
-) {
-    val items = localDataSource.loadItems()
-    val boxes = localDataSource.loadBoxes()
-    val collections = localDataSource.loadCollections()
-    val currentScreen = PackItUpRoute.SUMMARY
-
-    PackItUpAndroidTheme {
-        Surface {
-            PackItUpApp(
-                windowSize = WindowSizeClass.calculateFromSize(DpSize(412.dp, 732.dp)),
-                uiState = PackItUpUiState(
-                    items = items,
-                    boxes = boxes,
-                    collections = collections,
-                    currentScreen = currentScreen,
-                ),
-            )
-        }
-    }
-}
+// TODO: fix
+//@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+//@Preview(showBackground = true)
+//@Composable
+//fun PreviewPackItUp(
+//    localDataSource: LocalDataSource = LocalDataSource(),
+//) {
+//    val items = localDataSource.loadItems()
+//    val boxes = localDataSource.loadBoxes()
+//    val collections = localDataSource.loadCollections()
+//    val currentScreen = PackItUpRoute.SUMMARY
+//
+//    PackItUpAndroidTheme {
+//        Surface {
+//            PackItUpApp(
+//                windowSize = WindowSizeClass.calculateFromSize(DpSize(412.dp, 732.dp)),
+//                uiState = PackItUpUiState(
+//                    items = items,
+//                    boxes = boxes,
+//                    collections = collections,
+//                    currentScreen = currentScreen,
+//                ),
+//            )
+//        }
+//    }
+//}
