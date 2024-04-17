4653e0d709797a2201b363920c809cc7767c7481
CheoR <cheo.roman+github@gmail.com>
2024-04-17 08:45:09 -0500

    ref(model): remove unused import

--- app/src/main/java/com/example/packitupandroid/data/model/Summary.kt
+++ app/src/main/java/com/example/packitupandroid/data/model/Summary.kt
@@ -2,15 +2,14 @@ package com.example.packitupandroid.data.model
 
 import com.example.packitupandroid.utils.EditFields
 import java.util.Date
-import kotlin.collections.Collection
 
 data class Summary (
     override val id: String,
     override val name: String,
     override val description: String? = null,
-    val collections: List<Collection> = emptyList(),
-    val boxes: List<Box> = emptyList(),
     val items: List<Item> = emptyList(),
+    val boxes: List<Box> = emptyList(),
+    val collections: List<Collection> = emptyList(),
     override val isFragile: Boolean = items.any { it.isFragile },
     override val value: Double = items.sumOf { it.value },
     override val editFields: Set<EditFields> = emptySet(),
