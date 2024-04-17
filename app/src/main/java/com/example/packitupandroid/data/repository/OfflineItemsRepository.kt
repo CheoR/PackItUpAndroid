c8697cc3675ecfb4d2cebf757d83e24f64b2b48f
CheoR <cheo.roman+github@gmail.com>
2024-04-17 09:24:53 -0500

    ref(repository): remove unused imports

--- app/src/main/java/com/example/packitupandroid/data/repository/OfflineItemsRepository.kt
+++ app/src/main/java/com/example/packitupandroid/data/repository/OfflineItemsRepository.kt
@@ -2,10 +2,12 @@ package com.example.packitupandroid.data.repository
 
 import com.example.packitupandroid.data.database.dao.ItemDao
 import com.example.packitupandroid.data.database.entities.ItemEntity
-import com.example.packitupandroid.data.repository.ItemsRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.firstOrNull
 
 class OfflineItemsRepository(private val itemDao: ItemDao) : ItemsRepository {
+    override suspend fun getItem(id: String): ItemEntity? = itemDao.getItem(id).firstOrNull()
+
     override fun getAllItemsStream(): Flow<List<ItemEntity>> = itemDao.getAllItems()
 
     override fun getItemStream(id: String): Flow<ItemEntity?> = itemDao.getItem(id)
@@ -14,9 +16,11 @@ class OfflineItemsRepository(private val itemDao: ItemDao) : ItemsRepository {
 
     override suspend fun insertAll(items: List<ItemEntity>) = itemDao.insertAll(items)
 
+    override suspend fun updateItem(item: ItemEntity) = itemDao.update(item)
+
     override suspend fun deleteItem(item: ItemEntity) = itemDao.delete(item)
 
     override suspend fun deleteAll(items: List<ItemEntity>) = itemDao.deleteAll(items)
 
-    override suspend fun updateItem(item: ItemEntity) = itemDao.update(item)
+    override suspend fun clearAllItems() = itemDao.clearAllItems()
 }
