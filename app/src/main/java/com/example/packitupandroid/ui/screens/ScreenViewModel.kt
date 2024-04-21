768c56df6014a9ea6455e8ff235e27ed180c32d6
CheoR <cheo.roman+github@gmail.com>
2024-04-21 14:44:29 -0500

    build(screens): load summary data on app star

--- app/src/main/java/com/example/packitupandroid/ui/screens/ScreenViewModel.kt
+++ app/src/main/java/com/example/packitupandroid/ui/screens/ScreenViewModel.kt
@@ -3,6 +3,7 @@ package com.example.packitupandroid.ui.screens
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.packitupandroid.PackItUpUiState
+import com.example.packitupandroid.Result
 import com.example.packitupandroid.ui.navigation.PackItUpRoute
 import com.example.packitupandroid.ui.screens.box.BoxesScreenViewModel
 import com.example.packitupandroid.ui.screens.collection.CollectionsScreenViewModel
@@ -19,11 +20,38 @@ class ScreenViewModel(
     private val collectionViewModel: CollectionsScreenViewModel,
     private val summaryScreenViewModel: SummaryScreenViewModel,
 ) : ViewModel() {
+    private val isUseMockData = true
 
     private val _uiState = MutableStateFlow(PackItUpUiState())
     val uiState: StateFlow<PackItUpUiState> = _uiState.asStateFlow()
+    init {
+        viewModelScope.launch {
+            initializeUIState(isUseMockData)
+        }
+    }
 
-    // load Summary data here in init
+    private suspend fun initializeUIState(isUseMockData: Boolean = false) {
+        if (isUseMockData) {
+            viewModelScope.launch {
+                summaryScreenViewModel.uiState.collect { summaryUiState ->
+                    _uiState.value = summaryUiState
+                }
+            }
+        } else {
+            // TODO: REPLACE TO USE DIFFERENT DB
+            viewModelScope.launch {
+                try {
+                    summaryScreenViewModel.uiState.collect { summaryUiState ->
+                        _uiState.value = summaryUiState
+                    }
+                } catch (e: Exception) {
+                    _uiState.value = PackItUpUiState(
+                        result = Result.Error(e.message ?: "Unknown error")
+                    )
+                }
+            }
+        }
+    }
 
     fun loadCurrentScreenData(selectedScreen: String) {
         viewModelScope.launch {
