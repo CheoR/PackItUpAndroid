a66d64665007ca2f809b3c68d4ffeca84b86338e
CheoR <cheo.roman+github@gmail.com>
2024-04-28 10:52:57 -0500

    ref(screens): rename property names

--- app/src/main/java/com/example/packitupandroid/ui/screens/Screen.kt
+++ app/src/main/java/com/example/packitupandroid/ui/screens/Screen.kt
@@ -10,39 +10,37 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.packitupandroid.PackItUpUiState
 import com.example.packitupandroid.R
 import com.example.packitupandroid.Result
 import com.example.packitupandroid.data.model.BaseCardData
-import com.example.packitupandroid.data.model.Box
-import com.example.packitupandroid.data.source.local.LocalDataSource
 import com.example.packitupandroid.ui.components.card.BaseCard
 import com.example.packitupandroid.ui.components.counter.Counter
 import com.example.packitupandroid.ui.components.spinner.Spinner
-import com.example.packitupandroid.ui.navigation.PackItUpRoute
-import com.example.packitupandroid.utils.CardType
 
 @Composable
 fun <T: BaseCardData> Screen(
     uiState: PackItUpUiState,
-    createElement: (Int) -> Unit,
-    updateElement: (T) -> Unit,
-    destroyElement: (T) -> Unit,
-    card: @Composable (T, (T) -> Unit, (T) -> Unit) -> Unit,
+    onCreate: (Int) -> Unit,
+    onUpdate: (T) -> Unit,
+    onDestroy: (T) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    val rememberedElements: List<T>? = remember(uiState.result) {
-         when(uiState.result) {
-            is Result.Complete -> uiState.result.elements?.map { it as T }
+    val result = uiState.result
+
+    val rememberedElements: List<T> = remember(result) {
+        when (result) {
+            is Result.Complete -> {
+                result.elements as List<T>
+            }
             else -> emptyList()
         }
     }
 
     Column(modifier = modifier) {
-        when(uiState.result) {
+        when (result) {
             is Result.Loading -> Spinner()
-            is Result.Error ->  Error()
+            is Result.Error -> Error()
             is Result.Complete -> {
                 LazyColumn(
                     modifier = modifier.weight(1f),
@@ -51,7 +49,7 @@ fun <T: BaseCardData> Screen(
                     )
                 ) {
                     items(
-                        items = rememberedElements ?: emptyList(),
+                        items = rememberedElements,
                         key = { it.id }
                     ) { element ->
                         BaseCard(
@@ -69,6 +67,7 @@ fun <T: BaseCardData> Screen(
     }
 }
 
+
 @Composable
 private fun Error() {
     // TODO: move to utils
@@ -79,30 +78,4 @@ private fun Error() {
     ) {
         Text(text = "Screen Error")
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PreviewScreen(
-    localDataSource: LocalDataSource = LocalDataSource(),
-) {
-    val boxes = localDataSource.loadBoxes()
-    val uiState = PackItUpUiState(
-        currentScreen = PackItUpRoute.BOXES,
-        result = Result.Complete(boxes)
-    )
-    Screen<Box>(
-        uiState = uiState,
-        createElement = {},
-        updateElement = {},
-        destroyElement = {},
-        card = { data, update, destroy ->
-            BaseCard(
-                data = data,
-                onUpdate = update,
-                onDestroy = destroy,
-                cardType = CardType.Box,
-            )
-        },
-    )
 }
