801f1fe236c3f2301b7b90663d38c3c40f00fbca
CheoR <cheo.roman+github@gmail.com>
2024-04-28 10:51:16 -0500

    ref(card): exclude dropdown options for now

--- app/src/main/java/com/example/packitupandroid/ui/components/card/DataColumn.kt
+++ app/src/main/java/com/example/packitupandroid/ui/components/card/DataColumn.kt
@@ -4,18 +4,19 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.packitupandroid.data.source.local.LocalDataSource
 import com.example.packitupandroid.data.model.BaseCardData
 import com.example.packitupandroid.utils.CardType
 import com.example.packitupandroid.utils.EditMode
@@ -25,10 +26,16 @@ import com.example.packitupandroid.utils.asCurrencyString
 fun<T: BaseCardData> DataColumn(
     element: T,
     modifier: Modifier = Modifier,
-    dropdownOptions: List<String>? = listOf(""),
+//    getParentContainer: (T) -> BaseCardData?,
+//    getDropdownOptions: (T) -> List<BaseCardData>,
     editMode: EditMode = EditMode.NoEdit,
     cardType: CardType = CardType.Default,
 ) {
+
+    val expanded = remember { mutableStateOf(false) }
+//    val parent = getParentContainer(element) // dropdownOptions
+//    val dropdownOptions = getDropdownOptions(element)
+//    var currentSelection by remember { mutableStateOf(parent)}
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -41,17 +48,36 @@ fun<T: BaseCardData> DataColumn(
             modifier = Modifier
                 .fillMaxWidth(),
         )
-        dropdownOptions?.let {
-            BasicTextField(
-                value = dropdownOptions.first(), // Todo: display selected value if available else first
-                onValueChange = {},
-                textStyle = MaterialTheme.typography.bodySmall,
-                enabled = editMode == EditMode.Edit,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(4.dp)
-            )
-        }
+//        DropdownMenu(
+//            expanded = expanded.value,
+//            onDismissRequest = { expanded.value = false },
+//            offset = DpOffset(0.dp, (-150).dp),
+//            modifier = Modifier
+//                .background(MaterialTheme.colorScheme.inversePrimary),
+//        ) {
+//            dropdownOptions.forEach { option ->
+//                DropdownMenuItem(text = { Text(text = option.name )}, onClick = { currentSelection = option })
+//            }
+//            LazyColumn {
+//                items(
+//                    items = dropdownOptions,
+//                    key = { it.id },
+//                ) {
+//                    DropdownMenuItem(text = { Text(text = it.name) }, onClick = { currentSelection = it })
+//                }
+//            }
+//        }
+//        dropdownOptions.let {
+//            BasicTextField(
+//                value = currentSelection.name ?: "missing", // Todo: display selected value if available else first
+//                onValueChange = {},
+//                textStyle = MaterialTheme.typography.bodySmall,
+//                enabled = editMode == EditMode.Edit,
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//                    .padding(4.dp)
+//            )
+//        }
         BasicTextField(
             // TODO: fix
             value = element.description ?: "missing",
@@ -92,33 +118,33 @@ fun<T: BaseCardData> DataColumn(
 
 // TODO: fix
 
-@Preview(
-    showBackground = true,
-    group = "Default",
-)
-@Composable
-fun PreviewItemViewCardDataColumn(
-    localDataSource: LocalDataSource = LocalDataSource(),
-) {
-    val item = localDataSource.loadItems().first()
-    DataColumn(
-        data = item,
-    )
-}
-
-@Preview(
-    showBackground = true,
-    group = "Default",
-)
-@Composable
-fun PreviewBoxViewCardDataColumn(
-    localDataSource: LocalDataSource = LocalDataSource(),
-) {
-    val box = localDataSource.loadBoxes().first()
-    DataColumn(
-        data = box,
-    )
-}
+//@Preview(
+//    showBackground = true,
+//    group = "Default",
+//)
+//@Composable
+//fun PreviewItemViewCardDataColumn(
+//    localDataSource: LocalDataSource = LocalDataSource(),
+//) {
+//    val item = localDataSource.loadItems().first()
+//    DataColumn(
+//        element = item,
+//    )
+//}
+//
+//@Preview(
+//    showBackground = true,
+//    group = "Default",
+//)
+//@Composable
+//fun PreviewBoxViewCardDataColumn(
+//    localDataSource: LocalDataSource = LocalDataSource(),
+//) {
+//    val box = localDataSource.loadBoxes().first()
+//    DataColumn(
+//        element = box,
+//    )
+//}
 //@Preview(
 //    showBackground = true,
 //    group = "Default",
@@ -129,39 +155,39 @@ fun PreviewBoxViewCardDataColumn(
 //) {
 //    val collection = localDataSource.loadCollections().first()
 //    DataColumn(
-//        data = collection,
+//        element = collection,
 //    )
 //}
 
-@Preview(
-    showBackground = true,
-    group = "Summary",
-)
-@Composable
-fun PreviewItemSummaryCardDataColumn(
-    localDataSource: LocalDataSource = LocalDataSource(),
-) {
-    val item = localDataSource.loadItems().first()
-    DataColumn(
-        data = item,
-        cardType = CardType.Summary,
-    )
-}
-
-@Preview(
-    showBackground = true,
-    group = "Summary",
-)
-@Composable
-fun PreviewBoxSummaryCardDataColumn(
-    localDataSource: LocalDataSource = LocalDataSource(),
-) {
-    val box = localDataSource.loadBoxes().first()
-    DataColumn(
-        data = box,
-        cardType = CardType.Summary,
-    )
-}
+//@Preview(
+//    showBackground = true,
+//    group = "Summary",
+//)
+//@Composable
+//fun PreviewItemSummaryCardDataColumn(
+//    localDataSource: LocalDataSource = LocalDataSource(),
+//) {
+//    val item = localDataSource.loadItems().first()
+//    DataColumn(
+//        element = item,
+//        cardType = CardType.Summary,
+//    )
+//}
+//
+//@Preview(
+//    showBackground = true,
+//    group = "Summary",
+//)
+//@Composable
+//fun PreviewBoxSummaryCardDataColumn(
+//    localDataSource: LocalDataSource = LocalDataSource(),
+//) {
+//    val box = localDataSource.loadBoxes().first()
+//    DataColumn(
+//        element = box,
+//        cardType = CardType.Summary,
+//    )
+//}
 //@Preview(
 //    showBackground = true,
 //    group = "Summary",
@@ -172,7 +198,7 @@ fun PreviewBoxSummaryCardDataColumn(
 //) {
 //    val collection = localDataSource.loadCollections().first()
 //    DataColumn(
-//        data = collection,
+//        element = collection,
 //        cardType = CardType.Summary,
 //    )
 //}
