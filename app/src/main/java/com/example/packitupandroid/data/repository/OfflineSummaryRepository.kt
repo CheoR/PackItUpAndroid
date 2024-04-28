43ffdb74bddeeef9f1b9855d6a9f09303051dc30
CheoR <cheo.roman+github@gmail.com>
2024-04-28 15:07:25 -0500

    fix(repository): java.lang.IllegalStateException Cannot access database on the main thread since it may potentially lock the UI for a long period of time by making sure db operations are suspend functions.

--- app/src/main/java/com/example/packitupandroid/data/repository/OfflineSummaryRepository.kt
+++ app/src/main/java/com/example/packitupandroid/data/repository/OfflineSummaryRepository.kt
@@ -6,4 +6,5 @@ import kotlinx.coroutines.flow.Flow
 
 class OfflineSummaryRepository (private val summaryDao: SummaryDao): SummaryRepository {
     override fun getAllSummaryStream(): Flow<QuerySummary> = summaryDao.getSummary()
+    override suspend fun clearAllSummary() = summaryDao.clearAllSummary()
 }
