48c8a094c92874401268114db965e28db969e1b1
CheoR <cheo.roman+github@gmail.com>
2024-04-28 10:46:42 -0500

    build(app): change app ui state to interface instead of dataclass so each screen can have common ui state properties

--- app/src/main/java/com/example/packitupandroid/PackItUpUiState.kt
+++ app/src/main/java/com/example/packitupandroid/PackItUpUiState.kt
@@ -2,7 +2,6 @@ package com.example.packitupandroid
 
 import com.example.packitupandroid.data.model.BaseCardData
 import com.example.packitupandroid.data.model.Summary
-import com.example.packitupandroid.ui.navigation.PackItUpRoute
 
 sealed class Result {
     // TODO: Fix - look into making this into a Map<String, List<BaseCardData>>) : Result()
@@ -14,7 +13,7 @@ sealed class Result {
     data class Error(val message: String) : Result()
 }
 
-data class PackItUpUiState (
-    val currentScreen: String = PackItUpRoute.SUMMARY,
-    val result: Result = Result.Loading
-)
+interface PackItUpUiState {
+    val elements: List<BaseCardData>
+    val result: Result
+}
