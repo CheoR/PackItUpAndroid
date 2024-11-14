0f93f2f8df5d1bcf66462a4a334c1f25ccf131fc
CheoR <cheo.roman+github@gmail.com>
2024-11-14 14:48:01 -0600

    build: bump dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.packitupandroid"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.packitupandroid"
