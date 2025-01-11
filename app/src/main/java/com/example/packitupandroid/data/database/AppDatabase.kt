faf02f6339d931ce0653232a2c77c949b3f49ca5
CheoR <cheo.roman+github@gmail.com>
2025-01-11 05:26:01 -0600

    feat(database): Enhance AppDatabase mock data support
    This commit enhances the `AppDatabase` class by adding support for mock data.
    
    The following changes were made:
    
    -   The `getDatabase` method now accepts a `useMockData` boolean parameter to conditionally create the database from an asset.
    -   The database builder is now created outside the conditional block to avoid code duplication.
    -   Updated the comments to be more descriptive.
    
    These changes were made to:
    
    -   Improve code readability and maintainability by adding KDoc comments.
    -   Provide a way to easily create the database with or without mock data.
    -   Avoid code duplication by creating the database builder outside the conditional block.
    
    This commit enhances the structure and documentation of the database layer.

--- app/src/main/java/com/example/packitupandroid/data/database/AppDatabase.kt
+++ app/src/main/java/com/example/packitupandroid/data/database/AppDatabase.kt
@@ -15,42 +15,85 @@ import com.example.packitupandroid.data.database.entities.ItemEntity
 import com.example.packitupandroid.utils.Converters
 import com.example.packitupandroid.utils.DATABASE_NAME
 
+
 /**
  * Database class with singleton Instance object.
+ *
+ * @Database annotation:
+ * - version: Increase version number whenever the database table schema changes.
+ * - exportSchema: Set to false to avoid keeping schema version history backup.
+ *
+ * @TypeConverters annotation:
+ * - Specifies the Converters class to handle custom data types.
  */
-
 // version - increase version number whenever database table schema changes.
 // exportSchema - set as false as to not keep schema version history backup
 @Database(entities = [ItemEntity::class, BoxEntity::class, CollectionEntity::class], version = 5, exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
 
+    /**
+     * Provides access to the ItemDao.
+     *
+     * @return The ItemDao instance.
+     */
     abstract fun itemDao(): ItemDao
+
+    /**
+     * Provides access to the BoxDao.
+     *
+     * @return The BoxDao instance.
+     */
     abstract fun boxDao(): BoxDao
+
+    /**
+     * Provides access to the CollectionDao.
+     *
+     * @return The CollectionDao instance.
+     */
     abstract fun collectionDao(): CollectionDao
+
+    /**
+     * Provides access to the SummaryDao.
+     *
+     * @return The SummaryDao instance.
+     */
     abstract fun summaryDao(): SummaryDao
 
     companion object {
         @Volatile
         private var Instance: AppDatabase? = null
 
-        fun getDatabase(context: Context): AppDatabase {
-            // if the Instance is not null, return it, otherwise create a new
+        /**
+         * Returns the singleton instance of the AppDatabase.
+         *
+         * @param context The context used to create the database.
+         * @return The singleton instance of the AppDatabase.
+         */
+        fun getDatabase(
+            context: Context,
+            useMockData: Boolean,
+        ): AppDatabase {
+            // If the Instance is not null, return it, otherwise create a new
             // database instance.
-            // synchronized block means that only one thread of execution at
-            // time can enter this block of code, which makes sure database only
-            // gets initialized once.
+            // Synchronized block ensures that only one thread of execution at
+            // a time can enter this block of code, which ensures the database
+            // only gets initialized once.
             return Instance ?: synchronized(this) {
-                Room
+                val builder = Room
                     .databaseBuilder(context, AppDatabase::class.java, DATABASE_NAME)
-                    .createFromAsset("database/$DATABASE_NAME")
                     /**
-                     * Setting option in app's database builder means that Room
+                     * Setting this option in the app's database builder means that Room
                      * permanently deletes all data from database tables when it
                      * attempts to perform a migration with no defined migration path.
                      */
-                    // required migration strategy
                     .fallbackToDestructiveMigration()
+
+                if (useMockData) {
+                    builder.createFromAsset("database/$DATABASE_NAME")
+                }
+
+                builder
                     .build()
                     .also { Instance = it }
             }
