52376ba5912b05a46bbb38ce82ae4f7da30e0cb8
CheoR <cheo.roman+github@gmail.com>
2025-01-12 20:18:15 -0600

    docs(model): Add KDocs to BaseCardData interface
    This commit adds KDocs to the BaseCardData interface, providing documentation for each property and the interface itself. This improves code readability and maintainability.

--- app/src/main/java/com/example/packitupandroid/data/model/BaseCardData.kt
+++ app/src/main/java/com/example/packitupandroid/data/model/BaseCardData.kt
@@ -3,12 +3,49 @@ package com.example.packitupandroid.data.model
 import com.example.packitupandroid.utils.EditFields
 import java.util.Date
 
+
+/**
+ * Sealed interface representing a common entity in the inventory system.
+ */
 sealed interface BaseCardData {
+    /**
+     * Unique identifier for the entity.
+     */
     val id: String
+
+    /**
+     * Name of the entity.
+     */
     val name: String
+
+    /**
+     * Description of the entity. Can be null.
+     */
     val description: String?
+
+    /**
+     * Indicates if the entity is fragile.
+     */
     val isFragile: Boolean
+
+    /**
+     * The monetary value of the entity.
+     */
     val value: Double
-    val editFields: Set<EditFields>
+
+    /**
+     * Timestamp of the last modification of the entity.
+     */
     val lastModified: Date
+
+    /**
+     * A set of fields that can be edited in an item.
+     *
+     * This set contains [EditFields] objects, indicating which properties of an element
+     * are editable. The specific fields included in this set will vary depending on
+     * the type of element and the context in which it is being edited.
+     *
+     * @see EditFields
+     */
+    val editFields: Set<EditFields>
 }
