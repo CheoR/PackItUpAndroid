20444e0b5d5c937a6548f3556db7363da5458625
CheoR <cheo.roman+github@gmail.com>
2025-01-16 11:56:37 -0600

    feat(UI): Use generic Screen composable for all list screens
    This commit refactors BoxesScreen, CollectionsScreen and ItemsScreen to use the generic Screen composable for displaying lists, and removes the need for a custom `uiState` for each of these screens.
    
    The key changes include:
    - Replacing the custom `uiState` and its associated logic with a generic `result` property from the ViewModel's elements StateFlow.
    - Adding the following handlers: onCreate, onFieldChange and onUpdate, passed from the ViewModels.
    - Removing `onDestroy`, `getDropdownOptions`, `cardType` and `filterElements`.
    - Adding a key to the Screen composable, used for recomposition.
    - Removing the Screen specific preview composables.

--- app/src/main/java/com/example/packitupandroid/ui/screens/box/BoxesScreen.kt
+++ app/src/main/java/com/example/packitupandroid/ui/screens/box/BoxesScreen.kt
@@ -2,32 +2,58 @@ package com.example.packitupandroid.ui.screens.box
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.packitupandroid.Result
 import com.example.packitupandroid.data.model.Box
-import com.example.packitupandroid.data.model.Item
-import com.example.packitupandroid.data.model.QueryDropdownOptions
 import com.example.packitupandroid.data.source.local.LocalDataSource
 import com.example.packitupandroid.ui.PackItUpViewModelProvider
 import com.example.packitupandroid.ui.screens.Screen
-import com.example.packitupandroid.utils.CardType
+import com.example.packitupandroid.utils.Result
 
+
+/**
+ * Composable function that displays a screen with a list of boxes.
+ *
+- * This function uses a LazyColumn to display the boxes managed by the [BoxesScreenViewModel].
+- * It observes the boxes StateFlow from the ViewModel and updates the UI whenever the data changes.
++ * This function serves as the main entry point for displaying a list of boxes.
++ * It leverages the `BoxesScreenViewModel` to manage the box data and state, and uses the `Screen` composable
++ * to render the UI. It automatically updates the display when the underlying data changes.
++ *
++ * The function performs the following:
++ * 1. **Data Collection:** Collects the list of boxes from the `viewModel.elements` StateFlow using `collectAsState()`.
++ * 2. **UI Rendering:**  Utilizes the generic `Screen` composable to efficiently render the list of `Box` objects.
++ * 3. **Key Management:** Provides a unique key (`it?.id as String`) for each box to ensure efficient recomposition by Compose.
++ * 4. **Creation Handler:** Passes the `viewModel::create` function to the `Screen` composable to handle the creation of new boxes.
++ * 4. **FieldChange Handler:** Passes the `viewModel::onFieldChange` function to the `Screen` composable to handle changes to edit fields in [EditCard].
++ * 5. **State Observation:**  Reactively updates the UI whenever the `viewModel.elements` StateFlow emits a new list of boxes.
+ *
+ * @param viewModel The [BoxesScreenViewModel] used to manage the data and state. Defaults to a ViewModel provided by the [PackItUpViewModelProvider].Factory.
++ * @see Screen
++ * @see BoxesScreenViewModel
+ */
 @Composable
 fun BoxesScreen(
-    filterItemsByBoxId:((id: String) -> Unit)? = null,
-    getDropdownOptions: () -> List<QueryDropdownOptions>,
-    viewModel: BoxesScreenViewModel = viewModel(factory = PackItUpViewModelProvider.Factory),
+//    filterItemsByBoxId:((id: String) -> Unit)? = null,
+//    getDropdownOptions: () -> List<QueryDropdownOptions>,
+    viewModel: BoxesScreenViewModel = viewModel(factory = PackItUpViewModelProvider.Factory)
 ) {
-    val uiState = viewModel.uiState.collectAsState().value
+    val result by viewModel.elements.collectAsState()
+    val onCreate = viewModel::create
+    val onFieldChange = viewModel::onFieldChange
+    val onUpdate = viewModel::update
+
     Screen<Box>(
-        uiState = uiState,
-        onCreate = viewModel::create,
-        onUpdate = viewModel::update,
-        onDestroy = viewModel::destroy,
-        getDropdownOptions = getDropdownOptions,
-        cardType = CardType.Box,
-        filterElements = filterItemsByBoxId,
+        result = result,
+        key = { it?.id as String },
+        onCreate = onCreate,
+        onFieldChange = onFieldChange,
+        onUpdate = onUpdate,
+//        onDestroy = viewModel::destroy,
+//        getDropdownOptions = getDropdownOptions,
+//        filterElements = filterItemsByBoxId,
+//        cardType = CardType.Box,
     )
 }
 
@@ -37,15 +63,13 @@ fun PreviewItemsScreen(
     localDataSource: LocalDataSource = LocalDataSource(),
 ) {
     val boxes = localDataSource.loadBoxes()
-    val uiState = BoxesScreenUiState(
-        elements = boxes,
-        result = Result.Complete(boxes)
-    )
-    Screen<Item>(
-        uiState = uiState,
+    Screen<Box>(
+        key = { it?.id as String },
+        result = Result.Success(boxes),
         onCreate = {},
         onUpdate = {},
-        onDestroy = {},
-        getDropdownOptions = { emptyList() },
+        onFieldChange = {_, _, _ -> Unit},
+//        onDestroy = {},
+//        getDropdownOptions = { emptyList() },
     )
 }
--- app/src/main/java/com/example/packitupandroid/ui/screens/item/ItemsScreen.kt
+++ app/src/main/java/com/example/packitupandroid/ui/screens/item/ItemsScreen.kt
@@ -2,31 +2,60 @@ package com.example.packitupandroid.ui.screens.item
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.packitupandroid.Result
 import com.example.packitupandroid.data.model.Item
-import com.example.packitupandroid.data.model.QueryDropdownOptions
 import com.example.packitupandroid.data.source.local.LocalDataSource
 import com.example.packitupandroid.ui.PackItUpViewModelProvider
 import com.example.packitupandroid.ui.screens.Screen
-import com.example.packitupandroid.utils.CardType
+import com.example.packitupandroid.utils.Result
 
+
+/**
+ * Composable function that displays a screen with a list of boxes.
+ *
+- * This function uses a LazyColumn to display the boxes managed by the [ItemsScreenViewModel].
+- * It observes the boxes StateFlow from the ViewModel and updates the UI whenever the data changes.
++ * This function serves as the main entry point for displaying a list of boxes.
++ * It leverages the `BoxesScreenViewModel` to manage the box data and state, and uses the `Screen` composable
++ * to render the UI. It automatically updates the display when the underlying data changes.
++ *
++ * The function performs the following:
++ * 1. **Data Collection:** Collects the list of boxes from the `viewModel.elements` StateFlow using `collectAsState()`.
++ * 2. **UI Rendering:**  Utilizes the generic `Screen` composable to efficiently render the list of `Box` objects.
++ * 3. **Key Management:** Provides a unique key (`it?.id as String`) for each box to ensure efficient recomposition by Compose.
++ * 4. **Creation Handler:** Passes the `viewModel::create` function to the `Screen` composable to handle the creation of new boxes.
++ * 4. **FieldChange Handler:** Passes the `viewModel::onFieldChange` function to the `Screen` composable to handle changes to edit fields in [EditCard].
++ * 5. **State Observation:**  Reactively updates the UI whenever the `viewModel.elements` StateFlow emits a new list of boxes.
+ *
+ * @param viewModel The [ItemsScreenViewModel] used to manage the data and state. Defaults to a ViewModel provided by the [PackItUpViewModelProvider].Factory.
++ * @see Screen
++ * @see ItemsScreenViewModel
+ */
 @Composable
 fun ItemsScreen(
-    getDropdownOptions: () -> List<QueryDropdownOptions>,
+//    getDropdownOptions: () -> List<QueryDropdownOptions>,
     viewModel: ItemsScreenViewModel = viewModel(factory = PackItUpViewModelProvider.Factory),
 ) {
-    val uiState = viewModel.uiState.collectAsState().value
-    Screen(
-        uiState = uiState,
-        onCreate = viewModel::create,
-        onUpdate = viewModel::update,
-        onDestroy = viewModel::destroy,
-        getDropdownOptions = getDropdownOptions,
-        cardType = CardType.Item,
+    val result by viewModel.elements.collectAsState()
+    val onCreate = viewModel::create
+    val onUpdate = viewModel::update
+    val onFieldChange = viewModel::onFieldChange
+
+//        onDestroy = viewModel::destroy,
+//        getDropdownOptions = getDropdownOptions,
+
+    Screen<Item>(
+        result = result,
+        key = { it?.id as String },
+        onCreate = onCreate,
+        onFieldChange = onFieldChange,
+        onUpdate = onUpdate,
+        //        cardType = CardType.Box,
     )
 }
+
 
 @Preview(showBackground = true)
 @Composable
@@ -34,16 +63,15 @@ fun PreviewItemsScreen(
     localDataSource: LocalDataSource = LocalDataSource(),
 ) {
     val items = localDataSource.loadItems()
-    val uiState = ItemsPackItUpUiState(
-        elements = items,
-        result = Result.Complete(items)
-    )
+
     Screen<Item>(
-        uiState = uiState,
+        key = { it?.id as String },
+        result = Result.Success(items),
         onCreate = {},
         onUpdate = {},
-        onDestroy = {},
-        getDropdownOptions = { emptyList() },
-        cardType = CardType.Item,
+        onFieldChange = {_, _, _ -> Unit},
+//        onDestroy = {},
+//        getDropdownOptions = { emptyList() },
+//        cardType = CardType.Item,
     )
 }
