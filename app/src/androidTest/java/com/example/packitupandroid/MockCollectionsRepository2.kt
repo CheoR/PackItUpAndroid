1058c0fdbe1c9628e2861db4e5f864c4d38a58f5
CheoR <cheo.roman+github@gmail.com>
2025-02-20 11:15:39 -0600

    feat(test): Add MockCollectionsRepository2 for testing
    This commit introduces `MockCollectionsRepository2`, a new mock repository for collections, designed for use in testing.
    
    - Created `MockCollectionsRepository2.kt` as a testing-specific implementation of `CollectionsRepository`.
    - Implemented methods for getting, observing, inserting, updating, deleting, and clearing collections.
    - Added a mutable list and a state flow to manage and observe collection entities.
    - Added support to map between `CollectionEntity` and `Collection` data types.

---
+++ app/src/androidTest/java/com/example/packitupandroid/MockCollectionsRepository2.kt
@@ -0,0 +1,56 @@
+package com.example.packitupandroid
+
+import com.example.packitupandroid.data.database.entities.CollectionEntity
+import com.example.packitupandroid.data.model.Collection
+import com.example.packitupandroid.data.repository.CollectionsRepository
+import com.example.packitupandroid.data.repository.toCollection
+import com.example.packitupandroid.data.repository.toEntity
+import com.example.packitupandroid.utils.Result
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.map
+
+
+class MockCollectionsRepository2: CollectionsRepository {
+    private val collections = mutableListOf<CollectionEntity>()
+    private val collectionFlow = MutableStateFlow<List<CollectionEntity>>(emptyList())
+
+    override suspend fun get(id: String): Result<Collection?> {
+        return Result.Success(collections.find { it.id == id }?.toCollection())
+    }
+
+    override fun observeAll(): Flow<Result<List<Collection?>>> {
+        return collectionFlow.map { listOfCollectionEntities -> Result.Success(listOfCollectionEntities.map { it.toCollection() }) }
+    }
+
+    override fun observe(id: String): Flow<Result<Collection?>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun insert(data: List<Collection>): Result<Unit> {
+        this.collections.addAll(data.map { it.toEntity() })
+        collectionFlow.value = this.collections
+        return Result.Success(Unit)
+    }
+
+    override suspend fun update(data: Collection): Result<Unit> {
+        val index = collections.indexOfFirst { it.id == data.id }
+        if (index != -1) {
+            this.collections[index] = data.toEntity()
+            collectionFlow.value = this.collections
+        }
+        return Result.Success(Unit)
+    }
+
+    override suspend fun delete(data: List<String>): Result<Unit> {
+        this.collections.removeAll { data.contains(it.id) }
+        collectionFlow.value = this.collections
+        return Result.Success(Unit)
+    }
+
+    override suspend fun clear(): Result<Unit> {
+        this.collections.clear()
+        collectionFlow.value = this.collections
+        return Result.Success(Unit)
+    }
+}
