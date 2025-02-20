7a43d2d590ec77e2ae1ec90243b9bc83ac74899c
CheoR <cheo.roman+github@gmail.com>
2025-02-19 20:54:53 -0600

    refactor: Update repositories to use Result, observe, get, insert, update, delete, and clear
    This commit refactors the `MockBoxesRepository2` and `MockItemsRepository2` to align with recent updates in how the repositories are expected to interact with the data layer.
    
    - Implements `Result` state handling for `observe`, `get`, `insert`, `update`, `delete`, and `clear` functions.
    - Replaces individual item streams (like `getBoxStream`, `getItemStream`) with a single `observe` function that gets data for a single entity.
    - Replaces retrieving all items via a stream with `observeAll` function.
    - Removes methods like `insertAll`, `updateBox`, `deleteBox`, `deleteAll`, and `clearAllBoxes` and replaces them with `insert`, `update`, `delete`, `clear`.
    - Updates the parameters of `insert` to accept lists and not single items.
    - Adds `listOfBoxIdsAndNames` and `listOfCollectionIdsAndNames` to provide the list of available options for box and collection ids and names.
    - Changes `get` function to return a `Result` instead of the `Entity`.

--- app/src/androidTest/java/com/example/packitupandroid/MockItemsRepository2.kt
+++ app/src/androidTest/java/com/example/packitupandroid/MockItemsRepository2.kt
@@ -1,10 +1,24 @@
 package com.example.packitupandroid
 
 import com.example.packitupandroid.data.database.entities.ItemEntity
+import com.example.packitupandroid.data.model.BoxIdAndName
+import com.example.packitupandroid.data.model.Item
 import com.example.packitupandroid.data.repository.ItemsRepository
+import com.example.packitupandroid.data.repository.toEntity
+import com.example.packitupandroid.data.repository.toItem
+import com.example.packitupandroid.utils.Result
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
+
+
+private val boxesOptions = listOf(
+    BoxIdAndName("1", "Collection 1"),
+    BoxIdAndName("2", "Collection 2"),
+    BoxIdAndName("3", "Collection 3"),
+    BoxIdAndName("4", "Collection 4")
+)
 
 class MockItemsRepository2 : ItemsRepository {
     private val items = mutableListOf<ItemEntity>()
@@ -12,49 +26,46 @@ class MockItemsRepository2 : ItemsRepository {
 //    private val _uiState = MutableStateFlow(ItemsPackItUpUiState())
 //    val uiState: StateFlow<ItemsPackItUpUiState> = _uiState.asStateFlow()
 
-    override suspend fun getItem(id: String): ItemEntity? {
-        return this.items.find { it.id == id }
+    override fun listOfBoxIdsAndNames(): Flow<Result<List<BoxIdAndName?>>> {
+        return flowOf(Result.Success(boxesOptions))
     }
 
-    override fun getAllItemsStream(): Flow<List<ItemEntity>> {
-        return itemFlow
+    override suspend fun get(id: String): Result<Item?> {
+        return Result.Success(items.find { it.id == id }?.toItem())
     }
 
-    override fun getItemStream(id: String): Flow<ItemEntity?> {
-        return itemFlow.map { it.find { item -> item.id == id } }
+    override fun observeAll(): Flow<Result<List<Item?>>> {
+        return itemFlow.map { listOfItemEntities -> Result.Success(listOfItemEntities.map { it.toItem() }) }
     }
 
-    override suspend fun insertItem(item: ItemEntity) {
-        this.items.add(item)
-        itemFlow.value = this.items.toList()
+    override fun observe(id: String): Flow<Result<Item?>> {
+        TODO("Not yet implemented")
     }
 
-    override suspend fun insertAll(items: List<ItemEntity>) {
-        this.items.addAll(items)
-        itemFlow.value = this.items.toList()
-    }
-
-    override suspend fun updateItem(item: ItemEntity) {
-        val index = items.indexOfFirst { it.id == item.id }
-        if (index != -1) {
-            this.items[index] = item
-            itemFlow.value = this.items
-        }
-    }
-
-    override suspend fun deleteItem(item: ItemEntity) {
-        this.items.remove(item)
+    override suspend fun delete(data: List<String>): Result<Unit> {
+        this.items.removeAll { data.contains(it.id) }
         itemFlow.value = this.items
+        return Result.Success(Unit)
     }
 
-    override suspend fun deleteAll(items: List<ItemEntity>) {
-        this.items.removeAll(items)
-        itemFlow.value = this.items.toList()
-    }
-
-    override suspend fun clearAllItems() {
+    override suspend fun clear(): Result<Unit> {
         this.items.clear()
         itemFlow.value = this.items
+        return Result.Success(Unit)
     }
 
+    override suspend fun update(data: Item): Result<Unit> {
+        val index = items.indexOfFirst { it.id == data.id }
+        if (index != -1) {
+            this.items[index] = data.toEntity()
+            itemFlow.value = this.items
+        }
+        return Result.Success(Unit)
+    }
+
+    override suspend fun insert(data: List<Item>): Result<Unit> {
+        this.items.addAll(data.map { it.toEntity() })
+        itemFlow.value = this.items
+        return Result.Success(Unit)
+    }
 }
