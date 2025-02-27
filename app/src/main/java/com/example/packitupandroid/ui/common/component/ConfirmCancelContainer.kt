ecaa7f1e1724e07758a97e09d7223c42d251561b
CheoR <cheo.roman+github@gmail.com>
2025-02-27 10:01:09 -0600

    refactor(ui): Push dialog content up when keyboard is present with IME padding and dynamic width
    This commit enhances the `ConfirmCancelContainer` composable by adding padding for the Input Method Editor (IME) and making the width dynamic.
    
    - Added `windowInsetsPadding(WindowInsets.ime)` to `ConfirmCancelContainer` to ensure the dialog is properly padded when the keyboard is visible.
    - Modified `ConfirmCancelContainer` to no longer have a fixed width, allowing it to resize dynamically.

--- app/src/main/java/com/example/packitupandroid/ui/common/component/ConfirmCancelContainer.kt
+++ app/src/main/java/com/example/packitupandroid/ui/common/component/ConfirmCancelContainer.kt
@@ -4,9 +4,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -56,6 +59,7 @@ fun ConfirmCancelContainer(
         onDismissRequest = onCancel,
         modifier = modifier
 //            .requiredWidth(dialogWidth)
+            .windowInsetsPadding(WindowInsets.ime)
             .clip(shape = RoundedCornerShape(dimensionResource(R.dimen.roundness_x_small)))
             .background(MaterialTheme.colorScheme.background)
             .testTag("DialogContainer"),
