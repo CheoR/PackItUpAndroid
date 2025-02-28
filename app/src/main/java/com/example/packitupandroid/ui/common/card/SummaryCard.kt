599a19c190379416b1a2a6ac44f482ae49f624cc
CheoR <cheo.roman+github@gmail.com>
2025-02-28 06:58:32 -0600

    refactor(UI): Update SummaryCard background color
    - Updated the background color of `SummaryCard` to use `MaterialTheme.colorScheme.inversePrimary` for better theme integration.
    - Removed the previous `Color.LightGray` background.

--- app/src/main/java/com/example/packitupandroid/ui/common/card/SummaryCard.kt
+++ app/src/main/java/com/example/packitupandroid/ui/common/card/SummaryCard.kt
@@ -20,7 +20,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -56,7 +55,7 @@ fun SummaryCard(
     ) {
         Row(
             modifier = Modifier
-                .background(Color.LightGray)
+                .background(MaterialTheme.colorScheme.inversePrimary),
         ) {
             Column {
                 iconsContent()
