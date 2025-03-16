27b3d84e5e632716b3093da07aabc517e21a0dd6
CheoR <cheo.roman+github@gmail.com>
2025-03-16 13:30:31 -0500

    feat(test): Add utils for UI testing
    This commit introduces a set of utility functions and constants to support UI testing.
    
    - Added `ComposeMatcherUtils.kt` with matchers for content descriptions and test tags, along with helper functions for interacting with UI elements.
    - Added `Constants.kt` to define constants used in testing.
    - Added `AssertUtils.kt` to include helper methods for checking if an element is editable and counting child elements.
    - Added `SemanticInteractionUtils.kt` with functions for finding and interacting with specific UI components, like getting a random card ID or replacing text in a field.
    - Added functions to simplify test actions, such as incrementing/decrementing counters, adding elements, scrolling, opening menus, and making confirmation dialog selections.
    - Improved test setup and readability with the introduction of these utilities.

---
+++ app/src/androidTest/java/com/example/packitupandroid/utils/ComposeMatcherUtils.kt
@@ -0,0 +1,99 @@
+package com.example.packitupandroid.utils
+
+import androidx.compose.ui.semantics.SemanticsProperties
+import androidx.compose.ui.semantics.getOrNull
+import androidx.compose.ui.test.SemanticsMatcher
+import androidx.compose.ui.test.SemanticsNodeInteraction
+import androidx.compose.ui.test.filterToOne
+import androidx.compose.ui.test.hasContentDescription
+import androidx.compose.ui.test.junit4.ComposeTestRule
+import androidx.compose.ui.test.onChildren
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performScrollToIndex
+
+
+fun hasContentDescriptionThatEquals(contentDescription: String): SemanticsMatcher {
+    return SemanticsMatcher("ContentDescription equals $contentDescription") { node ->
+        val description = node.config.getOrNull(SemanticsProperties.ContentDescription)
+        description?.contains(contentDescription) == true
+    }
+}
+
+fun hasTestTagThatContains(substring: String): SemanticsMatcher {
+    // TODO: matcher works fine but can lead to false positives if multiple elements have
+    //  overlapping tags. Adding more specificity to the matcher logic could be useful,
+    //  especially in complex UI.
+    return SemanticsMatcher("TestTag contains $substring") { node ->
+        val testTag = node.config.getOrNull(SemanticsProperties.TestTag)
+        testTag?.contains(substring) == true
+    }
+}
+
+fun ComposeTestRule.performActionOnNode(
+    contentDescription: String,
+    times: Int,
+    action: SemanticsNodeInteraction.() -> SemanticsNodeInteraction
+) {
+    repeat(times) {
+        this.onNodeWithContentDescription(contentDescription)
+            .action()
+    }
+    this.mainClock.advanceTimeBy(DELAY)
+}
+
+fun ComposeTestRule.incrementCounter(times: Int) =
+    performActionOnNode("increment", times) { performClick() }
+
+fun ComposeTestRule.decrementCounter(times: Int) =
+    performActionOnNode("decrement", times) { performClick() }
+
+fun ComposeTestRule.clickAdd() =
+    performActionOnNode("add", 1) { performClick() }
+
+fun ComposeTestRule.confirmationDialogSelection(selection: String = "confirm") {
+    this
+        .onNodeWithText(selection)
+        .performClick()
+    this.mainClock.advanceTimeBy(DELAY)
+}
+
+fun ComposeTestRule.insertFiveItems() {
+    this.incrementCounter(incrementCountByFive)
+    this.clickAdd()
+    this.mainClock.advanceTimeBy(DELAY)
+}
+
+fun ComposeTestRule.insertFiveBoxes() {
+    incrementCounter(incrementCountByFive)
+    clickAdd()
+    this.mainClock.advanceTimeBy(DELAY)
+}
+
+
+fun ComposeTestRule.scrollToLastElement(elements: Int) {
+    // TODO: Add a check for no card here if needed
+    this
+        .onNodeWithTag("LazyColumn")
+        .performScrollToIndex(elements - 1)
+}
+
+fun ComposeTestRule.openMenuSelectionOption(card: SemanticsNodeInteraction, selection: String = "edit") {
+    openMenu(card)
+    this.mainClock.advanceTimeBy(DELAY * 2)
+    this
+        .onNodeWithText(selection)
+        .assertExists()
+        .performClick()
+    this.mainClock.advanceTimeBy(DELAY)
+}
+
+fun ComposeTestRule.openMenu(card: SemanticsNodeInteraction) {
+    card
+        .onChildren()
+        .filterToOne(hasContentDescription("Open Menu"))
+        .performClick()
+    this.mainClock.advanceTimeBy(DELAY)
+}
